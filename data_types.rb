aff4bf4e32c25fb70bcd2b3ab54c9bd4181677a1
Meka Asonye <meka@stripe.com>
2016-09-27 19:21:50 -0700

    Declared Var types

---
+++ data_types.rb
@@ -0,0 +1,14 @@
+stringvar = "This is a string"
+arrayvar = ["hello", "this", "is", "an", "array!"]
+booleanvar = true
+integervar = 5
+hashvar = {
+  exp_year: 2021,
+  exp_month: 07
+}
+
+puts stringvar
+puts arrayvar
+puts booleanvar
+puts integervar
+puts hashvar
